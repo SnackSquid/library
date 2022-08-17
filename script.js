4fc14f197a857b3d7f9b1dbb0f730fa98c4b68fe
Spencer Williams <Spilliams@gmail.com>
2022-08-17 15:26:46 -0500

    Remove default book

--- script.js
+++ script.js
@@ -9,18 +9,14 @@ function Book(title, author, pages, read) {
     this.info = () => `${title} by ${author}, ${pages} pages, ${read}.`
 }
 
-function makeBook() {
+function addBookToLibrary() {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
     const read = document.querySelector('input[name="read"]:checked').value;
 
     const newBook = new Book(title, author, pages, read);
-    return newBook;
-}
 
-function addBookToLibrary() {
-    const newBook = new Book("The Hobbit", "J.R.R. Tolkien", 255, "yes")
     bookArray.push(newBook);
     console.log(bookArray)
 }
@@ -30,4 +26,3 @@ function addBookToLibrary() {
 const submit = document.querySelector('#submit');
 submit.addEventListener('click', addBookToLibrary);
 
-console.log(bookArray)
