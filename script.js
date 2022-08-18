64efe4e7ab437d57cfdf6f5b5c4f76a193db39a2
Spencer Williams <spilliams@gmail.com>
2022-08-17 19:09:15 -0500

    Library will now add and display books

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let bookArray = [];
+let bookArray = [
+    {
+        title: 'The Hobbit',
+        author: 'JRR Tolkein',
+        pages: '300',
+        read: 'yes'
+    }
+];
+let bookCount = 0;
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-
-    this.info = () => `${title} by ${author}, ${pages} pages, ${read}.`
 }
 
 function addBookToLibrary() {
@@ -14,15 +20,54 @@ function addBookToLibrary() {
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
     const read = document.querySelector('input[name="read"]:checked').value;
+    const form = document.querySelector('form');
 
     const newBook = new Book(title, author, pages, read);
 
     bookArray.push(newBook);
-    console.log(bookArray)
+    makeCard();
+    form.reset();
+}
+
+function makeCard() {
+    const grid = document.querySelector('.bookGrid')
+
+    for (let i = bookCount; i < bookArray.length; i++) {
+        const div = document.createElement('div');
+        const title = document.createElement('h4');
+        const author = document.createElement('h4');
+        const pages = document.createElement('p');
+        const read = document.createElement('p');
+
+        for (let j = 0; j < 3; j++) {
+            title.textContent = bookArray[i].title;
+            author.textContent = bookArray[i].author;
+            pages.textContent = bookArray[i].pages;
+            read.textContent = bookArray[i].read;
+
+            div.classList = 'card';
+            div.appendChild(title);
+            div.appendChild(author);
+            div.appendChild(pages);
+            div.appendChild(read);
+            grid.appendChild(div);
+        }
+        
+        bookCount++;
+    }
 }
 
 
+function removeCard() {
 
+}
+
+function displayCards() {
+    
+}
+
+
+makeCard();
 const submit = document.querySelector('#submit');
 submit.addEventListener('click', addBookToLibrary);
 
